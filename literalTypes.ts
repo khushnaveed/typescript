693e9f855ff24805212708e6ebff47e21a9cd1c0
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-03-03 13:30:15 +0100

    add literal type

---
+++ literalTypes.ts
@@ -0,0 +1,6 @@
+//hovering on it shows the :string because using let, we can change the values but it has to be string
+let initials = "khush";
+
+//hovering on it shows  :"naveed" becauseusing const we cannot change the value of initials2 and it has to be "naveed" everywher in the code"
+const initials2 = "naveed";
+
