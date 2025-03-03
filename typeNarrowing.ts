955d2169262b38d1c7131614d1ad8a0a73fea0c1
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-03-03 14:02:17 +0100

    add type narrowing

---
+++ typeNarrowing.ts
@@ -0,0 +1,7 @@
+function check(value: boolean | number){
+    if(typeof value == "boolean"){
+        console.log("this is boolean value");
+    }else{
+        console.log("this is number value");
+    }
+}
